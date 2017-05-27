c86a02d9f67993af4d3ee73635420148b548fa87
Donald Hutchison <git@toastymofo.net>
2017-05-27 16:06:18 +0200

    More sprites.

--- Assets/Components/Lazer.cs
+++ Assets/Components/Lazer.cs
@@ -2,18 +2,32 @@ using UnityEngine;
 
 public class Lazer : MonoBehaviour
 {
+    private SpriteRenderer _renderer;
+    public Sprite active;
+    public Sprite inactive;
     /// <summary>
-    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
     /// </summary>
+    void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
     void Update()
     {
         if( Input.GetKey("space"))
         {
+            
+            _renderer.sprite = active;
             if(Random.value > 0.5)
             {
                 Vector3 cs = this.gameObject.transform.localScale;
                 this.gameObject.transform.localScale = new Vector3(cs.x * -1,cs.y, cs.z);
             }
+        }
+        else
+        {
+            _renderer.sprite = inactive;
         }
     }
 }
